92c9928b8385abf657f913effb8435580915b2e1
MSKA <mednologic@gmail.com>
2022-06-22 23:37:35 +0200

    "All done"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentComponent } from './content/content.component';
+import { StickyBelowViewDirective } from './sticky-below-view.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentComponent,
+    StickyBelowViewDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sticky-below-view.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[sticky-below-view]'
+})
+export class StickyBelowViewDirective {
+
+  constructor() { }
+
+}
